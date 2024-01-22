f4dd63517b6ca355158adbeccc5081bf96822ac2
AdamRse <bakaarion3@gmail.com>
2024-01-23 00:08:41 +0100

    fix init.php

--- php_config/init.php
+++ php_config/init.php
@@ -2,6 +2,8 @@
 session_start(); //Initialisation d'une session pour pouvoir utiliser $_SESSION
 
 // Constantes Path
+
+//SCRIPT_FILENAME sans l'index est un root plus fiable
 define("P_ROOT", $_SERVER['DOCUMENT_ROOT']);// Création d'une constante ROOT_PROJECT qui contiendra le chemin de la racine du projet. P_ nous indiquera que ce sont des constantes Path (chemin)
 define("P_BDD", P_ROOT."/php_config/Bdd.class.php");// Quand on voudra utiliser la Base de données, on aura plus qu'à appeler avec le chemin de P_BDD (par ex : require P_BDD)
 

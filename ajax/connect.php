1a0cf088230eeb9c5e16856ea0acfd892913d0b2
AdamRse <bakaarion3@gmail.com>
2024-01-24 13:32:33 +0100

    connection user

---
+++ ajax/connect.php
@@ -0,0 +1,21 @@
+<?php
+require "../php_config/init.php";
+
+//On initialise les variables de retour que va renvoyer returnAjax() en fin de script
+$retour = "Pramètres nom et mot de passe non reçu";
+$code = 2;
+
+//On récupère le pseudo et mot de passe envoyés
+if(!empty($_POST["name"]) && !empty($_POST['pass'])){
+    $user = $Bdd->connectUser($_POST["name"], $_POST["pass"]);
+    if($user){
+        $_SESSION['user']=$user;
+        if(empty($_COOKIE["TBD-cn"]))
+            setcookie('TBD-cn',$_SERVER['HTTP_USER_AGENT'].'!+'.$user['id'], time()+3600*10);
+    }
+    else{
+        $retour = "";
+    }
+
+}
+returnAjax($retour, $code);
--- ajax/example.php
+++ ajax/example.php
@@ -1,5 +1,2 @@
 <?php
 require "../php_config/init.php";
-include "../php_config/Bdd.class.php";
-$user=$Bdd->getUserName("Adam");
-var_dump($user);

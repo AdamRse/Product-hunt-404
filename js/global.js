0a8da1227c80f0d2e13b319c48b7cc1de6c10834
AdamRse <bakaarion3@gmail.com>
2024-01-25 15:00:58 +0100

    ajouter projet

--- js/global.js
+++ js/global.js
@@ -1,3 +1,6 @@
 //Fichier js appelé sur toutes les pages
 console.log("Hello global.js")
 
+document.querySelector("#btMenuCompte").addEventListener("click", function(){
+    document.querySelector("#menuCompte").classList.toggle("hidden");
+})
